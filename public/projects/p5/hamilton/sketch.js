2db6cf6956ce58bdb93e995967bfef08492829dc
mrstevenaweiss <weiss.steven@gmail.com>
2019-01-16 17:42:05 -0500

    added joints

--- public/projects/p5/hamilton/sketch.js
+++ public/projects/p5/hamilton/sketch.js
@@ -1,15 +1,54 @@
 var prob = 5000;
+var start;
+var started = false;
+var total = 0;
+
+var entrants;
 
 function setup() {
   noCanvas();
+
+  entrants = select('#entrants');
+  entrants.changed(updateProbability);
+
+  start = select('#start-button')
+  start.mousePressed(startLottery)
+  noLoop();
+
+  console.log('truly connected');
+  // alert(lyrics.alexander_hamilton);
 }
 
-function draw() {
-  var r = floor(random(5000));
+function updateProbability() {
+  prob = 50000 / entrants.html();
 
-  if (r === 1) {
-    console.log('you win');
-  } else {
-    console.log('lost');
+  select('#chance').html(prob)
+}
+
+function startLottery() {
+  started = true;
+  total = 0;
+  loop();
+}
+
+
+function draw() {
+  if (started) {
+    var r = floor(random(prob));
+
+    var results = select('#results')
+
+    if (r === 1) {
+      results.html('you won!')
+      noLoop();
+      // console.log('you won');
+    } else {
+      results.html('try again')
+      // console.log('you lost');
+    }
+  total++;
+  
+  select('#total').html(total);
   }
+
 }
